6b982d99804bc18c42020e775c6f0ac3446398c5
mojeed-painless <shittuola47@gmail.com>
2025-07-17 21:52:44 +0100

    commit

--- javascript_course/src/js/js_script.js
+++ javascript_course/src/js/js_script.js
@@ -9,7 +9,7 @@ head.forEach((item) => {
                 if (otherContent && otherContent.classList.contains('active')) {
                     otherContent.classList.remove('active');
                 }
-                // otherItem.classList.remove('active');
+                otherItem.classList.remove('active');
                 let otherArrow = otherItem.querySelector('.fa-angle-right, .fa-angle-down');
                 if (otherArrow && otherArrow.classList.contains('fa-angle-down')) {
                     otherArrow.classList.remove('fa-angle-down');
@@ -25,7 +25,7 @@ head.forEach((item) => {
         }
 
         // Toggle the 'active' class on the clicked head for color
-        // item.classList.toggle('active');
+        item.classList.toggle('active');
 
         // Toggle the arrow icon inside the clicked header
         let arrowIcon = item.querySelector('.fa-angle-right, .fa-angle-down');
